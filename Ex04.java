6353d4f7e7462b56f1418cb5d5b72e2baa7b8ded
svdjcuwg4638 <115617509+svdjcuwg4638@users.noreply.github.com>
2022-10-14 17:49:24 +0900

    Update Ex04.java

--- Ex04.java
+++ Ex04.java
@@ -2,38 +2,10 @@ package day04;
 
 public class Ex04 {
 	public static void main(String[] args) {
-		
-		String s1 = "abc@!#$..hjk";
-		
-		System.out.println(s1.contains("abc"));
-		System.out.println(s1.contains(".."));
-		
-		if(s1.contains("hjk")) {
-			s1 = s1.replace("hjk","xyz");
-		}
-		if(s1.contains(".")) {
-			s1 = s1.replace(".", "*");
-				
-		}
-			
-		System.out.println(s1);
-		
-		String s2 ="   Hello,world !!   ";
-		String s3 = s2.replace(" ", "");
-		System.out.println(s3);
-		
-		String s4 = s2.trim();
-		System.out.println(s4);
-		
-		String s5 ="    <div>\n        Hello\n    </div>    ";
-		System.out.println(s5);
-		
-		String s6 = s5.trim();	// ½ÃÀÛ°ú ³¡ÂÊÀÇ °ø¹éÀ» Á¦°Å
-		System.out.println(s6);
+
 		
 		
-		
-		// .Àº ¿¬¼ÓÀ¸·Î 2°³ ÀÌ»ó ¹èÄ¡µÉ ¼ö ¾ø´Ù
+		// .ì€ ì—°ì†ìœ¼ë¡œ 2ê°œ ì´ìƒ ë°°ì¹˜ë  ìˆ˜ ì—†ë‹¤ë¼ëŠ” ì‹ì„ ë§Œë“¤ê³  ì‹¶ì„ë•Œ
 		String s7 = "1.........2";
 //		if(s7.contains("...")) {
 		while(s7.contains("..")) {
@@ -42,21 +14,19 @@ public class Ex04 {
 		System.out.println(s7);
 		
 		String s8 = "abcABC123!@#...xyz";
-		// À§ ¹®ÀÚ¿­¿¡¼­ ¿µ¹®ÀÚ¸¦ Á¦¿ÜÇÑ ³ª¸ÓÁö´Â ¸ğµÎ Á¦°ÅÇÏ±â
+		// ìœ„ ë¬¸ìì—´ì—ì„œ ì˜ë¬¸ìë¥¼ ì œì™¸í•œ ë‚˜ë¨¸ì§€ëŠ” ëª¨ë‘ ì œê±°í•˜ê¸°
 		String s9 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLNMOPQRSTUVWXYZ";
 		String s10 = "";
 		
-		for(int i =0;i<s8.length();i++) {
-			char ch = s8.charAt(i);
-			if(s9.contains(ch+"")) {
-//				s10+=ch;
+		for(int i =0;i<s8.length();i++) {	
+			char ch = s8.charAt(i);		// s8ì˜ ë³€ìˆ˜ì— ì¸ë±ìŠ¤ì—ë§ëŠ” ê¸€ìë¥¼ chì— charí˜•íƒœë¡œ ì €ì¥
+			if(s9.contains(ch+"")) {	// containsëŠ” ë¬¸ìì—´ì— ì‚¬ìš©ê°€ëŠ¥í•˜ë‹ˆ ch+""ë¡œ ë¬¸ìì—´ë¡œ ë³€í™˜				
+//				s10+=ch;		// s9ì— í¬í•¨ëœ ë¬¸ìê°€ chì— ìˆë‹¤ë©´ s10ë¹ˆ ë¬¸ìì—´ì— chë„£ì–´ì£¼ê¸°
 				s10 = s10.concat(ch+"");
 			}
 		}
 		System.out.println(s10);
-		
-		int num = Integer.parseInt("1234");	//¹®ÀÚ¿­ ¼ıÀÚ¸¦ intÇü½ÄÀ¸·Î º¯Çü
-		System.out.println(num+1);
-		
+		//ê²°ê³¼ abcABCxyz
+
 	}
 }
